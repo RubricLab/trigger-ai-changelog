21f9e24e7d397ad9463de0e59908a9bda5df9296
tedspare <ted.spare@gmail.com>
2023-09-21 17:56:20 -0400

    Add gradient background

---
+++ app/components/Gradients.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Gradients = () => {
+  return (
+    <>
+      <div className="w-[60vw] h-[70vh] bg-purple-600 opacity-5 -rotate-6 rounded-full absolute top-1/6 left-1/4 blur-3xl z-[-1]" />
+      <div className="w-[60vw] h-[70vh] bg-blue-600 opacity-5 rotate-6 rounded-full absolute top-1/4 left-0 blur-3xl z-[-1]" />
+    </>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Dashboard } from "./components/Dashboard";
 import { Footer } from "./components/Footer";
+import { Gradients } from "./components/Gradients";
 import { Header } from "./components/Header";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center mx-auto space-y-16 px-4 md:px-12 pb-20 pt-16 max-w-7xl">
         <h1>Generate a Changelog using AI</h1>
         <Dashboard />
+        <Gradients />
       </div>
       <Footer />
     </main>
