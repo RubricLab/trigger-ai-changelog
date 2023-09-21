98f3ff1198008c6ac0403216c27e3d5dbf5d4742
tedspare <ted.spare@gmail.com>
2023-09-21 16:59:54 -0400

    Add explainer to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,22 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 w-screen h-10">
-      <div className="flex items-center justify-between h-full px-4 md:px-12 text-dimmed text-sm">
-        <div>
-          Built on{" "}
-          <Link href="https://trigger.dev" target="_blank">
-            Trigger.dev
-          </Link>{" "}
-          by{" "}
-          <Link href="https://rubric.sh" target="_blank">
-            Rubric Labs
-          </Link>
-          .
+    <footer className="fixed bottom-0 left-0 w-screen h-14 border-t">
+      <div className="flex items-center justify-between h-full px-4 text-dimmed text-sm">
+        <div className="flex gap-2">
+          <div className="text-primary font-semibold">How does it work?</div>{" "}
+          <div className="text-sm">
+            This site is powered by{" "}
+            <Link href="https://trigger.dev" target="_blank">
+              Trigger.dev
+            </Link>{" "}
+            , an open source background jobs framework.
+          </div>
         </div>
-        <p>Copyright 2023.</p>
+        <Link
+          href="https://github.com/rubriclab/trigger-ai-changelog"
+          target="_blank"
+        >
+          <Button
+            variant="secondary"
+            size="sm"
+            className="text-dimmed space-x-1"
+          >
+            <span>Explore the code</span>
+            <ArrowRight className="w-4 h-4" />
+          </Button>
+        </Link>
       </div>
     </footer>
   );
