045f2e42449bd6b6fce52fbf5d7d7e8f2a95085f
tedspare <ted.spare@gmail.com>
2023-09-22 15:16:56 -0400

    Fix header link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,16 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { TriggerLogo } from "./logos/Trigger";
 import { GitHub } from "./logos/GitHub";
+import { baseUrl } from "../constants";
 
 export const Header = () => {
   return (
     <header className="w-screen h-14 border-b">
       <div className="flex items-center justify-between h-full px-4 md:px-12">
         <div className="flex items-baseline gap-2">
-          <Link href="/">
+          <Link href={baseUrl}>
             <div className="text-2xl font-bold tracking-tighter">
               <span className="bg-gradient-to-r from-indigo-500 to-purple-500 text-transparent bg-clip-text">
                 AutoChangelog
--- app/constants.ts
+++ app/constants.ts
@@ -1,1 +1,5 @@
 export const githubUrl = "https://github.com/triggerdotdev/trigger.dev";
+export const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? "localhost:3000"
+    : "autochangelog.rubric.sh";
